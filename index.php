b3f06b1282fd4a8daf022598812148c10dab7940
Jeremias ramirez <jereramirez499@gmail.com>
2019-10-24 11:06:13 -0400

    Agregando blur

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" ng-app="post" class="">
+<html lang="en" ng-app="post" class="blurtime">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
